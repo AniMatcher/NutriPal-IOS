5a4a35a0226f0404922e92e49b74115f335385ee
Philip Yi <philip.yi@duke.edu>
2023-09-12 23:22:20 -0400

    Initial Commit

---
+++ NutriPal/NutriPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriPalApp.swift
+//  NutriPal
+//
+//  Created by Philip Yi on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NutriPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
